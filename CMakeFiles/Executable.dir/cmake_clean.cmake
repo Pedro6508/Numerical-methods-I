b8337aec14df00e8f811aa762a1fd66b69cf665a
gustavovzqz <gustavovazquez@alu.ufc.br>
2023-10-21 19:04:23 -0300

    carlos

---
+++ CMakeFiles/Executable.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Executable.dir/src/function.cpp.o"
+  "CMakeFiles/Executable.dir/src/function.cpp.o.d"
+  "CMakeFiles/Executable.dir/src/main.cpp.o"
+  "CMakeFiles/Executable.dir/src/main.cpp.o.d"
+  "CMakeFiles/Executable.dir/src/output.cpp.o"
+  "CMakeFiles/Executable.dir/src/output.cpp.o.d"
+  "CMakeFiles/Executable.dir/src/sonic_airplane.cpp.o"
+  "CMakeFiles/Executable.dir/src/sonic_airplane.cpp.o.d"
+  "bin/Executable"
+  "bin/Executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
