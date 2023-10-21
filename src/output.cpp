e6715d769d228f4ec40cf68e1b08cb24246d0e72
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-10-21 18:22:01 -0300

    fix: fixing number rows from productToOrders

--- src/output.cpp
+++ src/output.cpp
@@ -18,18 +18,22 @@ size_t result::MaxKeyLength(const std::map<std::string, T> &m) {
 void result::run() {
     constexpr size_t Rows = 5;
     const std::map<std::string, std::array<double, Rows>> productToOrders{
-            {"apples",   {100, 200, 50.5, 30,   10}},
-            {"bananas",  {80,  10,  100,  120,  70}},
-            {"carrots",  {130, 75,  25,   64.5, 128}},
-            {"tomatoes", {70,  100, 170,  80,   90}}
+            {"LABEL A",   {100, 200, 50.5, 30,   10}},
+            {"LABEL B",  {80,  10,  100,  120,  70}},
+            {"LABEL C",  {130, 75,  25,   64.5, 128}},
+            {"LABEL D", {70,  100, 170,  80,   90}},
+            {"LABEL E",   {100, 200, 50.5, 30,   10}},
+            {"LABEL F",  {80,  10,  100,  120,  70}},
+            {"LABEL G",  {130, 75,  25,   64.5, 128}},
+            {"LABEL H", {70,  100, 170,  80,   90}},
     };
 
     int colLength = MaxKeyLength(productToOrders) + 2;
-    int NumRows = MaxKeyLength(productToOrders);
+    int mapRows = productToOrders.size();
     for (const auto& name : std::views::keys(productToOrders))
         std::cout << fmt::format("{:>{}}", name, colLength);
     std::cout << '\n';
-    for (size_t i = 0; i < NumRows; ++i) {
+    for (size_t i = 0; i < mapRows; ++i) {
         for (const auto& values : std::views::values(productToOrders)) {
             std::cout << fmt::format("{:>{}.2f}", values[i], colLength);
         }
