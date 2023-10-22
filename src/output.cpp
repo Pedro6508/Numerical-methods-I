7dc490ef515a54b171fb529a8856f6ead5185792
gustavovzqz <gustavovazquez@alu.ufc.br>
2023-10-21 21:10:57 -0300

    Adicionando a "raiz exata" na tabela.

--- src/output.cpp
+++ src/output.cpp
@@ -16,7 +16,7 @@ void Output::inputFunction(double a, double e1){
     double bis = ap.bisection();
     double newton = ap.newtonRaphson();
     double false_pos = ap.falsePosition();
-    std::vector<double> comp = {a, e1, floor(exp(a)), ceil(exp(a)),  bis, (double) ap.getIBis(), false_pos, (double) ap.getIPos(), newton, (double) ap.getINewt()};
+    std::vector<double> comp = {a, e1, floor(exp(a)), ceil(exp(a)),  bis, (double) ap.getIBis(), false_pos, (double) ap.getIPos(), newton, (double) ap.getINewt(), exp(a)};
     comparativeData.push_back(comp);
     std::vector<double> result = {a, e1, bis, false_pos, newton};
     resultData.push_back(result);
@@ -26,7 +26,7 @@ void Output::inputFunction(double a, double e1){
 
 void Output::resultTable(){
      // Printa
-    std::cout << std::left << std::setw(5) << " a" << std::setw(10) << " erro" << std::setw(15) << " bisseção" 
+    std::cout << std::left << std::setw(5) << " a" << std::setw(10) << " acc " << std::setw(15) << " bisseção" 
               << std::setw(18) << "posição falsa" << std::setw(10) << "     newton " << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
 
@@ -38,16 +38,17 @@ void Output::resultTable(){
 }
 
 void Output::comparativeTable(){ // Printa
-   std::cout << " a       erro        intervalo     bisseção          posição falsa      newton " << std::endl;
-   std::cout << "-------------------------------------------------------------------------------" << std::endl;
+   std::cout << " a       acc         intervalo     bisseção          posição falsa      newton      raiz esperada" << std::endl;
+   std::cout << "--------------------------------------------------------------------------------------------------" << std::endl;
 
      for (int i = 0; i < comparativeData.size(); i++) {
         std::cout << " " << std::left << std::setw(8) << comparativeData[i][0] << 
-        std::setw(12) << comparativeData[i][1] << std::setw(15) << "[" + std::to_string((int)(comparativeData[i][2]))
+        std::setw(12) << comparativeData[i][1] << std::setw(14) << "[" + std::to_string((int)(comparativeData[i][2]))
         + "," + std::to_string((int)comparativeData[i][3]) + "]" << 
         std::setw(19) << std::to_string (comparativeData[i][4]) + " " + std::to_string((int) comparativeData[i][5]) 
         << std::setw(17) << std::to_string (comparativeData[i][6]) + " " + std::to_string((int) comparativeData[i][7])
-        << std::to_string(comparativeData[i][8]) + " " + std::to_string((int) comparativeData[i][9]) <<std::endl; 
+        << std::setw(16) << std::to_string(comparativeData[i][8]) + " " + std::to_string((int) comparativeData[i][9]) 
+        <<  comparativeData[i][10] << std::endl; 
     }
 }
 
